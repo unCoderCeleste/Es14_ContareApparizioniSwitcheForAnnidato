e1f45ceb1d4fe12ff33942248407101704c1b0b2
EmiSysCode <120025747+EmiSysCode@users.noreply.github.com>
2022-12-21 20:29:52 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,42 +18,34 @@ public class Main {
         for (int i = 0; i < elementi.length; i++) {
             switch (elementi[i]) {
                 case 0:
-                    contatore[0]++;
-                    break;
+                    contatore[0]++; break;
                 case 1:
-                    contatore[1]++;
-                    break;
+                    contatore[1]++; break;
                 case 2:
-                    contatore[2]++;
-                    break;
+                    contatore[2]++; break;
                 case 3:
-                    contatore[3]++;
-                    break;
+                    contatore[3]++; break;
                 case 4:
-                    contatore[4]++;
-                    break;
+                    contatore[4]++; break;
                 case 5:
-                    contatore[5]++;
-                    break;
+                    contatore[5]++; break;
                 case 6:
-                    contatore[6]++;
-                    break;
+                    contatore[6]++; break;
                 case 7:
-                    contatore[7]++;
-                    break;
+                    contatore[7]++; break;
                 case 8:
-                    contatore[8]++;
-                    break;
+                    contatore[8]++; break;
                 case 9:
-                    contatore[9]++;
-                    break;
+                    contatore[9]++; break;
             }
         }
+        // STAMPARE IL CONTATORE DEL SWITCH:
         System.out.println("\nLeggo quante volte compare una cifra: ");
         System.out.println("'Usando SWITCH':");
         for (int i = 0; i < contatore.length; i++) {
             System.out.println(i + "---->" + contatore[i]);
         }
+        // CONTATORE USANDO SWITCH:
         int[] contatore2 = new int[10];
         for (int i = 0; i < elementi.length; i++) {
             for (int j = 0; j < contatore2.length; j++) {
@@ -62,6 +54,7 @@ public class Main {
                 }
             }
         }
+        // STAMPARE IL CONTATORE DEL FOR ANNIDATO:
         System.out.println("\n'Usando FOR ANNIDATO':");
         for (int i = 0; i < contatore2.length; i++) {
             System.out.println("Il numero " + i + " compare: " + contatore2[i] + " volte");
